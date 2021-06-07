4ccdeafee7e74fcaa28334bc93c6076af63fbdf4
utopier <utopier2025@gmail.com>
2021-06-07 18:24:58 +0900

    Backend axios baseURL Update

--- store/sagas/index.ts
+++ store/sagas/index.ts
@@ -4,8 +4,7 @@ import axios from 'axios';
 import postSaga from './post';
 import userSaga from './user';
 
-axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? "https://lwdc6kzck0.execute-api.ap-northeast-2.amazonaws.com":'http://localhost:2025';
-// axios.defaults.baseURL = "http://13.124.80.203:2025";
+axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? "https://utopier.ml":'http://localhost:2025';
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
